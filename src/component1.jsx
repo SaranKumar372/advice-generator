06334e1ab1c2233a797e395dd891af3aefdfe9ed
SaranKumar372 <sarankumar.6136@gmail.com>
2025-03-08 22:24:48 +0530

    Update component1.jsx

--- src/component1.jsx
+++ src/component1.jsx
@@ -4,13 +4,11 @@ import divider from "./assets/pattern-divider-desktop.svg";
 import dice from "./assets/icon-dice.svg";
 
 function Component1() {
-
-    const [component, setComponent] = useState(null); // Default to null
+    const [component, setComponent] = useState(null); 
         async function apiCall() {
             try {
                 const response = await fetch ("https://api.adviceslip.com/advice");
                 const data = await response.json();
-    
                 setComponent({
                     id: data.slip.id,
                     advice: data.slip.advice,
@@ -21,43 +19,21 @@ function Component1() {
                 console.error("Error fetching advice:", error);
             }
         }
-
         useEffect(() => {
             apiCall();
         },[])
-    /*
-
-    useEffect(() => {
-        setTimeout(() => (
-            setComponent([
-                {
-                    id : 1,
-                    advice : "fhi ie ejig",
-                    divider : divider ,
-                    dice : dice,
-                }
-            ])
-        ),2000)
-    },[]);
-
-    */
-    
-
     if(!component) {
         return (
             <div className="loading-container">
                 {<img className="loading" src="src\assets\827.gif" />}  
             </div>
         )
-
     }
-
     return (
         <>
             <Component2 key={component.id} id={component.id} advice={component.advice} divider={component.divider} dice={component.dice} />
         </>
     );
 }
-
 export default Component1;
 
